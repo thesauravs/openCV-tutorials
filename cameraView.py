915b006126b0ab92a15645d2d82d7afee5e54743
Saurav Shah <thesaurav.sh@gmail.com>
2019-10-20 10:16:03 +0530

    Camera with OpenCV

---
+++ cameraView.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+cap = cv.VideoCapture(0);
+
+while True:
+    ret, frame = cap.read()
+    
+    cv.imshow('frames', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllwindows()
+cv.waitKey(1)
